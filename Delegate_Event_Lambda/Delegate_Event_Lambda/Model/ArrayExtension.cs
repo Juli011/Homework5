5c9f19f513a663b78d20d2d74eec2b3186b2a1c2
uuusss631@gmail.com <uuusss631@gmail.com>
2023-03-24 22:14:05 +0200

    update

---
+++ Delegate_Event_Lambda/Delegate_Event_Lambda/Model/ArrayExtension.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegate_Event_Lambda.Model
+{
+    internal static class ArrayExtension
+    {
+        public static double Root(this int number)
+        {
+              return Math.Sqrt(number);
+        }
+    }
+}
